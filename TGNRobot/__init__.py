f2d3d3a955f802fe4f6b03e488b35cbb393d65a2
KUNAL12459 <94036274+KUNAL12459@users.noreply.github.com>
2021-11-20 05:10:47 +0530

    Update __init__.py

--- TGNRobot/__init__.py
+++ TGNRobot/__init__.py
@@ -114,7 +114,7 @@ if ENV:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
 else:
-    from TGNRobot.config import Development as Config
+    from FlorinaRobot.config import Development as Config
 
     TOKEN = Config.TOKEN
 

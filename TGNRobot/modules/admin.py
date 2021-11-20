40243711d1f17de82a1c2614861a9efba132b23c
Sumans11 <88786146+Sumans11@users.noreply.github.com>
2021-11-20 07:58:37 +0530

    Update admin.py

--- TGNRobot/modules/admin.py
+++ TGNRobot/modules/admin.py
@@ -5,9 +5,9 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from TGNRobot import DRAGONS, dispatcher
-from TGNRobot.modules.disable import DisableAbleCommandHandler
-from TGNRobot.modules.helper_funcs.chat_status import (
+from FlorinaRobot import DRAGONS, dispatcher
+from FlorinaRobot.modules.disable import DisableAbleCommandHandler
+from FlorinaRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_pin,
     can_promote,
@@ -15,19 +15,19 @@ from TGNRobot.modules.helper_funcs.chat_status import (
     user_admin,
     ADMIN_CACHE,
 )
-from TGNRobot.helper_extra.admin_rights import (
+from FlorinaRobot.helper_extra.admin_rights import (
     user_can_pin,
     user_can_promote,
     user_can_changeinfo,
 )
 
-from TGNRobot.modules.helper_funcs.extraction import (
+from FlorinaRobot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from TGNRobot.modules.log_channel import loggable
-from TGNRobot.modules.helper_funcs.alternate import send_message
-from TGNRobot.modules.helper_funcs.alternate import typing_action
+from FlorinaRobot.modules.log_channel import loggable
+from FlorinaRobot.modules.helper_funcs.alternate import send_message
+from FlorinaRobot.modules.helper_funcs.alternate import typing_action
 
 
 @run_async

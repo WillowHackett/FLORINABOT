54de1f8f5d8d6ccd20ba9c64eafa0834a804284d
Sumans11 <88786146+Sumans11@users.noreply.github.com>
2021-11-20 07:51:29 +0530

    Update __speachtotext.py

--- TGNRobot/modules/__speachtotext.py
+++ TGNRobot/modules/__speachtotext.py
@@ -1,4 +1,4 @@
-from TGNRobot import telethn as tbot
+from FlorinaRobot import telethn as tbot
 import os
 import urllib.request
 from datetime import datetime
@@ -11,8 +11,8 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from TGNRobot import *
-from TGNRobot.events import register
+from FlorinaRobot import *
+from FlorinaRobot.events import register
 
 
 async def is_register_admin(chat, user):

526c56c2a96d0e2f040f999313b5350c600978d8
Sumans11 <88786146+Sumans11@users.noreply.github.com>
2021-11-20 05:54:28 +0530

    Update __encrypt.py

--- TGNRobot/modules/__encrypt.py
+++ TGNRobot/modules/__encrypt.py
@@ -1,6 +1,6 @@
-from TGNRobot import telethn as tbot
+from FlorinaRobot import telethn as tbot
 import os
-from TGNRobot.events import register
+from FlorinaRobot.events import register
 import secureme
 
 @register(pattern="^/encrypt ?(.*)")
